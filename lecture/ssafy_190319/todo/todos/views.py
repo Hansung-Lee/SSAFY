82d55d586b3b4a95811c2990d7df13b9c5fd8b3f
Hansung <ajtwlsgkst@naver.com>
2019-03-19 11:54:42 +0900

    add ssafy_190319

---
+++ lecture/ssafy_190319/todo/todos/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from .models import Todo
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+
+def index(request):
+    return redirect('todos:home')
+
+
+def home(request):
+    if request.user.is_authenticated:
+        # todos = T`odo.objects.filter(user_id=request.user).order_by('completed', '-id')
+        todos = request.user.todo_set.order_by('completed', '-id')
+        return render(request, 'todos/home.html', {
+            'todos': todos
+        })
+    else:
+        todos = None
+        return render(request, 'todos/home.html', {
+            'todos': todos
+        })
+
+
+def create(request):
+    # todos 작성하기
+    Todo.objects.create(content=request.POST.get('content'), user_id=request.user.id)
+
+    return redirect('todos:home')
+
+
+def check(request, todo_id):
+    # 특정 id를 가진 TODO를 뽑아 Completed = True
+    todo = Todo.objects.get(id=todo_id)
+    if todo.completed:
+        todo.completed = False
+    else:
+        todo.completed = True
+    todo.save()
+
+    return redirect('todos:home')
+
+
+def delete(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    todo.delete()
+    return redirect('todos:home')
---
+++ lecture/ssafy_190319/todo/users/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+# Create your views here.
+# '/users/' => 홈페이지
+# '/users/login' => 로그인 화면
+
+
+def home(request):
+    return render(request, 'users/home.html')
+
+
+def login_user(request):
+    # /users/login GET  -> 로그인 창을 render
+    # /users/login POST  -> 로그인(유저 검증)
+        # 만약에 username, password 넘어온 값이 DB에 저장된 값과 같다면,
+        #     로그인
+        # 아니면,
+        #     실패
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # 1. 사용자가 로그인 되었을 때
+            #
+            messages.success(request, "로그인 성공")
+            return redirect('todos:home')
+        else:
+            # 2. 로그인이 되지 않았을 때
+            messages.warning(request, '로그인 실패, 다시 시도해 주세요.')
+            return redirect('users:login')
+
+    else:
+        return render(request, 'users/login.html')
+
+
+def logout_user(request):
+    # 유저를 로그아웃 시킨다.
+    logout(request)
+    # 유저에게 로그아웃이 되었다는 메세지를 전달한다.
+    messages.success(request, "로그아웃 되었습니다.")
+    return redirect('todos:home')
